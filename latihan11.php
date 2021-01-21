94a9f69f969e640cf02723e49b77c0be7c5c953d
yansa05 <myansa46@gmail.com>
2021-01-21 19:55:44 +0700

    Menambah file latihan 11

---
+++ latihan11.php
@@ -0,0 +1,22 @@
+<?php 
+/* Pertemuan 11 PHP Dasar 
+Perulangan 
+for, while, dan do while */
+
+for ($i=0; $i < 5; $i++){
+    echo "Helo World <br>" ;
+}
+
+$i = 0;
+while ($i < 5){
+    echo "Hello World <br>";
+    $i++;
+}
+
+$j = 0;
+do {
+    echo "Hello world";
+    $j++;
+}while ($j < 5);
+
+?>
