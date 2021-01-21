9beec14aa428a7351d1c0099b142d5d00ce62ece
yansa05 <myansa46@gmail.com>
2021-01-21 08:56:27 +0700

    Menambahkan file pertemuan ke 5

---
+++ pertemuan5.php
@@ -0,0 +1,36 @@
+<?php 
+/* Pertemuan 5 Dasar PHP
+Operator Aritmatika */
+$nilaiA = 10;
+$nilaiB = 3;
+$hasil;
+
+echo "Penjumlahan : ";
+echo "\n";
+$hasil = $nilaiA + $nilaiB;
+var_dump($hasil);
+
+echo "Pengurangan : ";
+echo "\n";
+$hasil = $nilaiA - $nilaiB;
+var_dump($hasil);
+
+echo "Perkalian : ";
+echo "\n";
+$hasil = $nilaiA * $nilaiB;
+var_dump($hasil);
+
+echo "Pembagian : ";
+echo "\n";
+$hasil = $nilaiA / $nilaiB;
+var_dump($hasil);
+
+
+echo "Sisa bagi : ";
+echo "\n";
+$hasil = $nilaiA % $nilaiB;
+var_dump($hasil);
+
+
+
+?>
