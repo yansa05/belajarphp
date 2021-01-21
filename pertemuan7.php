f0e96b7303f06ef06beb99d34c79cc1b9c8c6c9d
yansa05 <myansa46@gmail.com>
2021-01-21 09:30:59 +0700

    Menambahkan file pertemuan ke 7

---
+++ pertemuan7.php
@@ -0,0 +1,13 @@
+<?php 
+/* Pertemuan 7 Dasar PHP
+Operator Perbandingan */
+
+var_dump(10 == 10);
+var_dump("10" === "10");
+var_dump(10 > 10);
+var_dump(9 < 10 );
+var_dump(10 != 9);
+var_dump(10 >= 10);
+var_dump(9 <= 9);
+
+?>
