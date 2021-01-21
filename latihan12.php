a9d6483644b60133dbbd7e0fb859e041b9317a36
yansa05 <myansa46@gmail.com>
2021-01-21 20:24:25 +0700

    Menambahkan file  latihan 12

---
+++ latihan12.php
@@ -0,0 +1,42 @@
+<?php 
+/* Pertemuan 12 Dasar PHP
+Pengkondisian
+if else, if else if , tenary,  switch 
+*/
+$i = 10;
+
+if ($i < 11){
+    echo "Benar";
+}
+
+echo "\n";
+
+$j = 5;
+if ($j <=4 ){
+    echo "benar";
+}else {
+    echo "salah";
+    }
+
+echo "\n";  
+
+$nilai = 8;
+if ($nilai >= 9){
+    echo "Amat Baik";
+}else if($nilai >= 7 ){
+    echo "Baik";
+}else if($nilai >= 5){
+    echo "Kurang";
+}else {
+    echo "Buruk";
+
+}
+
+
+
+
+
+
+
+
+?>
