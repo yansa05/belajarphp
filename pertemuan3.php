c22b256766aa5e29557f713f1b9610214036d273
yansa05 <myansa46@gmail.com>
2021-01-21 08:21:16 +0700

    Membuat file pertemuan ke 3

---
+++ pertemuan3.php
@@ -0,0 +1,23 @@
+<?php 
+/* Pertemuan 3 Dasar PHP
+Tipe Data */
+
+// Tipe Data Number
+echo "Int : ";
+var_dump(30);
+
+echo "Float : ";
+var_dump(9.5);
+
+// Tipe Data Boolean
+echo "Benar : ";
+var_dump(true);
+
+echo "Salah : ";
+var_dump(false);
+
+// Tipe Data String 
+echo "String : ";
+var_dump("Yansa");
+
+?>
