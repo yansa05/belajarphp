182f70f1837fece4563ce205d8d4850c59442529
yansa05 <myansa46@gmail.com>
2021-01-21 09:57:48 +0700

    Menambah file pertemuan ke 9

---
+++ pertemuan9.php
@@ -0,0 +1,15 @@
+<?php 
+/* Pertemuan 9 Dasar PHP
+Increment dan decrement */
+// Increment
+$nilaiA =1;
+
+$nilaiA++;
+var_dump($nilaiA);
+
+// Decrement
+$nilaiB = 10;
+$nilaiB--;
+var_dump($nilaiB);
+
+?>
