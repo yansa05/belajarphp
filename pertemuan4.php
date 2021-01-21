fdc47c6753be5c5f183072da3901f2e8e2924418
yansa05 <myansa46@gmail.com>
2021-01-21 08:41:42 +0700

    Menambah file pertemuan ke 4

---
+++ pertemuan4.php
@@ -0,0 +1,16 @@
+<?php 
+/* Pertemuan 4 Dasar PHP
+variable */
+$nama = "Yansa";
+$umur = 30;
+
+echo "Nama : ";
+echo "\n";
+echo $nama;
+echo "\n";
+echo "Umur : ";
+echo "\n";
+echo $umur;
+
+
+?>
